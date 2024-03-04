47768da5dda3c284936e7a54ca394237390beebb
Marcus Vinícius Tibúrcio <127891536+macustiburcio23@users.noreply.github.com>
2024-03-04 13:54:20 -0300

    Create question-03.java

---
+++ question-03.java
@@ -0,0 +1,11 @@
+public class Programa3 {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 45;
+        int c = 67;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
